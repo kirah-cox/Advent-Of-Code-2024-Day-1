76cf1076c0b546d9b163e0742e8495a649f1146e
kirah-cox <kirahanncox@gmail.com>
2025-02-03 17:13:50 -0700

    Add project files.

---
+++ Advent Of Code 2024 Day 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using MyTools;
+
+public class Program
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\kirah.cox\\source\\repos\\Advent Of Code 2024 Day 1\\Advent Of Code 2024 Day 1\\input.txt";
+        List<string> inputList = File.ReadAllLines(filePath).ToList();
+
+        List<string[]> newInputList = new List<string[]>();
+        foreach (string input in inputList)
+        {
+            newInputList.Add(input.Split("   "));
+        }
+        
+        List<string> firstColumn = new List<string>();
+        List<string> secondColumn = new List<string>();
+        foreach (string[] line in newInputList)
+        {
+            firstColumn.Add(line[0]);
+            secondColumn.Add(line[1]);
+        }
+
+        List<int> parsedFirstColumn = new List<int>();
+        foreach (string column in firstColumn)
+        {
+            parsedFirstColumn.Add(int.Parse(column));
+        }
+
+        List<int> parsedSecondColumn = new List<int>();
+        foreach (string column in secondColumn)
+        {
+            parsedSecondColumn.Add(int.Parse(column));
+        }
+
+        parsedFirstColumn.Sort();
+        parsedSecondColumn.Sort();
+
+        List<int> addColumns = new List<int>();
+        int lineNumber = 0;
+        foreach (int column in parsedFirstColumn)
+        {
+            addColumns.Add(Tools.FindDifferenceInColumns(parsedFirstColumn[lineNumber], parsedSecondColumn[lineNumber]));
+            lineNumber++;
+        }
+
+        int result = addColumns.Sum();
+        Console.WriteLine(result);
+    }
+}
+
---
+++ Advent Of Code 2024 Day 1/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTools
+{
+    public class Tools
+    {
+        public static int FindDifferenceInColumns(int parsedFirstColumn, int parsedSecondColumn)
+        {
+            return Math.Abs(parsedFirstColumn - parsedSecondColumn);
+        }
+    }
+}
